77e4ed97364c27a4bff26390f16981c8244090b3
Rafael Pires <rafael.pires@unine.ch>
2018-09-23 21:45:07 +0000

    fix term color

--- negative/App/App.cpp
+++ negative/App/App.cpp
@@ -13,7 +13,7 @@ int ocall_print_string(const char *str) {
     /* Proxy/Bridge will check the length and null-terminate 
      * the input string to prevent buffer overflow. 
      */
-    return printf("\033[34,1mEnclave says\033[0m: %s", str);
+    return printf("\033[34;1mEnclave says\033[0m: %s", str);
 }
 
 
